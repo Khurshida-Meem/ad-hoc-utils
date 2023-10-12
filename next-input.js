15c92ff1e436a7985b9dcf8ea0cbf975acfa71b4
Khurshida Jahan Meem <meemrumi59@gmail.com>
2023-10-11 22:40:31 -0700

    Create next-input.js

---
+++ next-input.js
@@ -0,0 +1,35 @@
+//auto focus on next input field after giving a value to prev input field
+
+const handleChange = (e: any, field: any) => {
+    setFieldValue(field, e.target.value);
+
+    const { maxLength, name } = e.target;
+
+    const [fieldName, fieldIndex] = name.split("_");
+
+    let fieldIntIndex = parseInt(fieldIndex, 10);
+
+    if (e.target.value.length >= maxLength) {
+      if (fieldIntIndex >= 1) {
+        const nextfield = document.querySelector(
+          `input[name=field_${fieldIntIndex + 1}]`
+        );
+
+        if (nextfield !== null) {
+          //@ts-ignore
+          nextfield.focus();
+        }
+      }
+    } else if (e.target.value.length === 0) {
+      if (fieldIntIndex >= 1) {
+        const prevfield = document.querySelector(
+          `input[name=field_${fieldIntIndex - 1}]`
+        );
+
+        if (prevfield !== null) {
+          //@ts-ignore
+          prevfield.focus();
+        }
+      }
+    }
+  };
